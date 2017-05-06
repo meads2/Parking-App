3ef6e7f026ccc2246c9a868b007ece2afa928ee3
Matt Eads <matteads2@gmail.com>
2017-05-06 13:53:16 -0400

    Added Dashboard

--- js/module-test.js
+++ js/module-test.js
@@ -29,33 +29,33 @@ var App = {
       lat: App.config.airport_lat,
       lng: App.config.airport_lng,
       zoom: App.config.map_zoom,
-      styles: App.config.custom_map_settings,
-      click: function(e){
-        console.log(e.latLng);
-      }
+      styles: App.config.custom_map_settings
     });
   },
   getData: function(){
     $.getJSON(App.config.api, function(data){
-      App.addOverlays(data.garages)
+      App.addOverlays(data.garages);
       return data.garages
     });
   },
   addOverlays: function(data){
     $.each(data, function(key, value){
-      console.log(value);
+      // console.log(value);
       // Get Percent of Spots available
       var freeSpaces = parseInt(value.spaces_available);
       var totalSpaces = parseInt(value.total_spaces);
       var percentFree = Math.round((freeSpaces/totalSpaces) * 100);
-      console.log(percentFree);
+      // console.log(percentFree);
       // HTML Marker & Popup
       // Show Class Depending On Value of Percent Free
       if(percentFree > 10){
         App.map.drawOverlay({
           lat: value.lat,
           lng: value.lng,
-          content: '<div class="dott"><div class="mark blue"></div><div class="dot-mk blue pulse"></div></div>'
+          content: '<div class="dott"><div class="mark blue"></div><div class="dot-mk blue pulse"></div></div>',
+          mouseover: function(e){
+            alert('Working...');
+          }
         });
       }else{
         App.map.drawOverlay({
@@ -340,17 +340,25 @@ var Sidebar = {
       // Pagination dots
       this.$dots = this.$el.find('.dot');
       // card
-      this.$card = this.$el.find('.card');
+      this.$cards = this.$el.find('.card');
       // Main Button
       this.$main_btn = this.$el.find('.btn-primary');
       // Skip tutorial Button
       this.$btn_sub = this.$el.find('.btn-sub');
+      // Forward Btn
+      this.$fwd = this.$el.find('.fwd');
+      // Back Btn
+      this.$back = this.$el.find('.back');
     },
     eventBinder: function(){
       // Main Button Clicked
       this.$main_btn.click(this.nextPanel.bind(this));
       // Sidebar Closed
       this.$btn_sub.click(this.closeSidebar.bind(this));
+      // Fwd Btn Clicked
+      this.$fwd.click(this.nextPanel.bind(this));
+      // Back Btn Clicked
+      this.$fwd.click(this.prevPanel.bind(this));
     },
     nextPanel: function(e){
       // Get current index
@@ -360,15 +368,14 @@ var Sidebar = {
           this.$el.addClass('animated fadeOutLeft');
       }
       // Fade Out That Card
-      this.$card.eq(index).addClass('animated fadeOutLeft');
+      this.$cards.eq(index).addClass('animated fadeOutLeft');
       // Add Active class to next dot
       this.$dots.removeClass('active');
       this.$dots.eq(index + 1).addClass('active');
     },
     prevPanel: function(e){
-      alert('prev panel');
       var index = $('.dot.active').index();
-      this.$card.eq(index-1).addClass('animated fadeInRight');
+      this.$cards.eq(index-1).addClass('animated fadeInRight');
     },
     closeSidebar: function(){
       this.$el.addClass('animated fadeOutLeft');
@@ -380,22 +387,54 @@ var Sidebar = {
 
 Sidebar.init();
 
+// Dashboard
+var Dashboard = {
+  init: function(){
+    this.getData();
+    this.updateDash();
+  },
+  getData: function(){
+    $.getJSON('https://api.phila.gov/airport-parking/v1/', function(data){
+      Dashboard.updateDash(data.garages);
+    });
+  },
+  updateDash: function(data){
+    console.log('dashboard updated...');
+    // Loop through lots
+    $.each(data, function(key, value){
+      // Get Percent of Spots available
+      var freeSpaces = parseInt(value.spaces_available);
+      var totalSpaces = parseInt(value.total_spaces);
+      var percentFree = Math.round((freeSpaces/totalSpaces) * 100);
+      var lotName = value.display_name;
+      // Create Lot LI
+      var lot = $('<li/>');
+      var metrics = $('<div class="metrics"/>');
+      var name = $('<h1>');
+      name.append(lotName);
+      // Add Free Spaces
+      var freeSpace = $('<span class="free"/>');
+      freeSpace.text(freeSpaces);
 
-// function sbContentToggle(){
-//   $('.btn-primary').click(function(){
-//     if($(this).text() == 'How It Works?'){
-//       $(this).parent().addClass('animated fadeOutLeft');
-//       $('.dot').eq(1).addClass('active').siblings().removeClass('active');
-//     }else if($(this).text() == 'View Parking') {
-//       $(this).parent().addClass('animated fadeOutLeft')
-//       $('.dot').eq(2).addClass('active').siblings().removeClass('active');
-//     }else{
-//       $('.sb').addClass('animated fadeOutLeft');
-//     }
-//   });
-//   $('.btn-sub').click(function(){
-//     $('.sb').addClass('animated fadeOutLeft');
-//   });
-// };
-//
-// sbContentToggle();
+      // Add Handicap Spaces
+      var adaSpaces = $('<span class="ada"/>');
+      adaSpaces.text(totalSpaces);
+
+      // Add Percent Free
+      var perFree = $('<span class="perFree"/>');
+      perFree.text(percentFree);
+
+      lot.append(name);
+      metrics.append(freeSpace);
+      metrics.append(adaSpaces);
+      metrics.append(perFree);
+
+      lot.append(metrics);
+      // Append LI to lot list
+      $('#lots').append(lot);
+
+    });
+  },
+}
+
+Dashboard.init();
